3d8745078eeaa5e1a1e541109fa91b4b0cf2d8d2
bikdm12 <bikdm12@gmail.com>
2020-03-27 20:29:41 +0300

    Add NumPy style docstrings.

--- rodeo_utils/rodeo2antismash.py
+++ rodeo_utils/rodeo2antismash.py
@@ -1,3 +1,9 @@
+"""Convert sequence in a common genbank format to genbank output of antiSMASH.
+
+The main function is convert_gbk(...).
+
+"""
+
 import os
 import csv
 import re
@@ -8,10 +14,28 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 
 
 def check_if_border(feature, operon_borders):
+    """Return start/end coordinate of the SeqFeature if its accession is the first/last in the given tuple.
+
+    Parameters
+    ----------
+    feature : SeqFeature
+        Directory with input genbank files.
+    operon_borders : str
+        Directory to store the output.
+        
+    Returns
+    -------
+    tuple(str, int)
+        The first value is either 'end' or 'start'.
+        The second value is the corresponding coordinate.
+        
+        If SeqFeature's ID is not in the given tuple, returns None. 
     
+    """
     prot_id_regexp = re.compile('[A-Z]{2}_[0-9]+\.[0-9]')
         
-    start_id, end_id = operon_borders
+    start_id = operon_borders[0]
+    end_id = operon_borders[-1]
     
     if 'protein_id' in feature.qualifiers:
                     
@@ -47,9 +71,35 @@ def check_if_border(feature, operon_borders):
 
 
 
-def convert_gbk(gb_dir, gb_out_dir, rodeo_output, bg_domains, n, product_class):
+def convert_gbk(gb_dir, gb_out_dir, rodeo_output, bg_domains, max_intergenic_distance = 100, product_class = 'thiopeptide'):
+    """Convert a common genbank file to the genbank antiSMASH output.
     
-    rodeo_output.table_proccessing(bg_domains, n)
+    Adds a feature “cluster” with information about the class of the product.
+    The coordinates of this feature are boundaries of the group of adjacent genes on the same strand that includes RODEO query.
+    Marks genes with given domains as biosynthetic.
+
+    Parameters
+    ----------
+    gb_dir : str
+        Directory with input genbank files.
+    gb_out_dir : str
+        Directory to store the output.
+    rodeo_output: RodeoOutput
+        RODEO output to use as a reference.
+    bg_domains : list
+        List of Pfam or TIGRFAMs IDs for domains that are important for your product biosynthesis.
+    max_intergenic_distance : int, optional
+        Maximum distance (nt) between genes within the biosynthetic gene cluster (default: 100).
+    product_class : string, optional
+        A putative class of the final product (default: thiopeptide).
+    
+    Returns
+    -------
+    bool
+        True if successful, False otherwise.
+    
+    """
+    rodeo_output.table_proccessing(bg_domains, max_intergenic_distance)
     operon_border_accs = (rodeo_output.operon_accs[0], rodeo_output.operon_accs[-1])
     biosynthetic_genes = rodeo_output.biosynthetic_genes
     
@@ -79,6 +129,8 @@ def convert_gbk(gb_dir, gb_out_dir, rodeo_output, bg_domains, n, product_class):
             record.features = [cluster] + record.features
             
             SeqIO.write(record, '%s%s.gbk' % (gb_out_dir, prot_id), 'genbank')
+            return True
     
     except Exception as e:
         print e
+        return False
--- rodeo_utils/rodeo_utils.py
+++ rodeo_utils/rodeo_utils.py
@@ -1,3 +1,8 @@
+"""Main module of the package.
+Provides RODEO output iterator and a class to store information for a single query.
+Also contains a number of simple, but useful functons. 
+
+"""
 import os
 import csv
 import re
@@ -6,15 +11,20 @@ from Bio import SeqIO
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 
 class RodeoOutput(object):
-    
+    """Object that stores rodeo output for a single query as attributes.
+
+    """    
     def __init__(self, table):
-        
         self.query = table[0][0]
+        """str: Accession number of the query protein."""
         self.table = table
+        """list of list: Corresponding part of rodeo output main_co_occur.csv table. Contains rows of the table as lists."""
+        # TODO: Add attributes: lists for left and rigth query neighbours.
     
     def table_proccessing(self, bg_domains, n):
-    
+        # TODO: Split into two methods. 
         self.biosynthetic_genes = []
+        """list of str: List of proteins (accession numbers) that contain domains from a given list"""
                 
         operon_buffer = []
         operon_domain_buffer = []
@@ -38,8 +48,9 @@ class RodeoOutput(object):
             else:
                 if self.query in operon_buffer:
                     self.operon_accs = operon_buffer
+                    """list of str: Group of protein accession numbers that includes query. Genes of these proteins form so called quasioperon: they are adjacent and located on the same strand."""
                     self.operon_domains = operon_domain_buffer
-                
+                    """list of str: List of Pfam or TIGRFAMs IDs corresponding to proteins from quasioperon."""
                 operon_buffer = [row[3]]
                 operon_domain_buffer = [domain]        
             
@@ -50,9 +61,45 @@ class RodeoOutput(object):
             self.operon_accs = operon_buffer
             self.operon_domains = operon_domain_buffer
 
+class RodeoDirError(Exception):
+    """Exception to raise if rodeo_output_iterator failed to infer the structure of the given directory.
 
+    """
+    def __init__(self, rod_dir):
+        self.rod_dir = rod_dir
+    def __str__(self):
+        return 'Failed to infer the structure of %s directory. It must be either RODEO output or rodout/ folder of the RIPPER output.' % self.rod_dir
 
-def rodeo_output_iterator(rod_dir, rod_dir_type):
+def rodeo_output_iterator(rod_dir):
+    """Iterate over RODEO output.
+
+    Parameters
+    ----------
+    rod_dir : str
+        Path to RODEO output.
+
+    Yields
+    ------
+    RodeoOutput
+        An object with the results for a single query.
+
+    Raises
+    --------
+    RodeoDirError
+        If the given directory is neither RODEO or RIPPER output
+
+    """
+    rod_dir_type = None
+    if 'main_co_occur.csv' in os.listdir(rod_dir):
+        rod_dir_type = 'RODEO'
+    else:
+        for folder in os.listdir(rod_dir):
+            if os.path.isdir('%s/%s' % (rod_dir, folder)):
+                if 'main_co_occur.csv' in os.listdir('%s/%s' % (rod_dir, folder)):
+                    rod_dir_type = 'RIPPER'
+                    break
+        if rod_dir_type is None:
+            raise RodeoDirError(rod_dir)
     
     if rod_dir_type == 'RODEO':
         # It woulod be easier to use Pandas, but this way is more memory-efficient.
@@ -88,6 +135,19 @@ def rodeo_output_iterator(rod_dir, rod_dir_type):
 
 
 def split_file(file_path, n = 1000):
+    """Split a file into chunks with a given number of lines in each.
+    
+    Resulting files are stored in the same directory as input.
+    Useful to split files for RODEO since it doesn't accept more than 1000 accession numbers. 
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the file to split.
+    n : int, optional
+        The number of lines in each chunk.
+    
+    """
     with open(file_path, 'r') as infile:
         inlist = [x[0] for x in csv.reader(infile)]
     
